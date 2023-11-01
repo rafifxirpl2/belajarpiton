22c31abccb24563afe7be3e550e748edacfe88c2
rafifxirpl2 <140592197+rafifxirpl2@users.noreply.github.com>
2023-11-01 14:20:14 +0700

    Add files via upload

---
+++ piton.py
@@ -0,0 +1,10 @@
+# Deklarasi variabel
+person1 = "Sherlock"
+person2 = "Enola"
+location = "the office"
+
+# Membuat kalimat
+sentence = f"{person1} and {person2} are going to {location}"
+
+# Menampilkan kalimat
+print(sentence)
